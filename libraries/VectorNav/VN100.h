834133d748f8a2e0cbbd089b85137e6ea4e910ea
Avik De <avikde@gmail.com>
2017-04-29 19:31:01 -0400

    VN100 uses DMA now but still request wait read

--- libraries/VectorNav/VN100.h
+++ libraries/VectorNav/VN100.h
@@ -29,8 +29,9 @@ typedef struct {
  * @brief VN100 hardware interface library
  */
 class VN100 {
-  SPIClass& theSPI;
+  SPIClass& _SPI;
   uint8_t csPin;
+  uint8_t cmd[4];
 public:
   // response header for SPI read/write commands
   uint8_t resphead[4];
@@ -38,9 +39,9 @@ public:
 
   /**
    * @brief Construct with reference to which SPI
-   * @param theSPI SPI, SPI_2, etc.
+   * @param _SPI SPI, SPI_2, etc.
    */
-  VN100(SPIClass& theSPI) : theSPI(theSPI) {}
+  VN100(SPIClass& _SPI) : _SPI(_SPI) {}
   
   /**
    * @brief Send initialization commands to the VN100
@@ -53,12 +54,16 @@ public:
     pinMode(csPin, OUTPUT);
     digitalWrite(csPin, HIGH);
 
-    theSPI.begin();
-    // APB1 on F303 has prescaler 2 => 36MHz
+    _SPI.begin();
+    // APB1 on F303 has prescaler 2 => 36MHz => div2 = 18MHz
     // VN100 says 16MHz max SPI speed
-    theSPI.setClockDivider(SPI_CLOCK_DIV2);
-    theSPI.setBitOrder(MSBFIRST);
-    theSPI.setDataMode(SPI_MODE3);
+    _SPI.setClockDivider(SPI_CLOCK_DIV2);
+    _SPI.setBitOrder(MSBFIRST);
+    _SPI.setDataMode(SPI_MODE3);
+
+    // Use DMA
+    _SPI.initDMA(RCC_AHBPeriph_DMA1, DMA1_Channel5, DMA1_Channel4, DMA1_FLAG_TC5, DMA1_FLAG_TC4);
+    cmd[2] = cmd[3] = 0;//other two are set each time
 
     // Initial configuration
     // VPE config (register 35) defaults are 1,1,1,1 - OK
@@ -67,44 +72,7 @@ public:
     writeReg(VN_REG_VPE_MAG_CONFIG, 36, (const uint8_t *)magConfig);
 
     // Test read and request (avoid wait)
-    // readReg(240, 0, NULL, VN_REQUEST);
-
-    // Use DMA
-    // RCC_AHBPeriphClockCmd(RCC_AHBPeriph_DMA1, ENABLE);
-    // DMA_DeInit(DMA1_Channel4);
-    // DMA_DeInit(DMA1_Channel5);
-    // DMA_InitTypeDef     DMA_InitStructure;
-    // // DMA for reading
-    // // Configure SPI_BUS RX Channel
-    // DMA_InitStructure.DMA_DIR = DMA_DIR_PeripheralSRC; // From SPI to memory
-    // DMA_InitStructure.DMA_PeripheralBaseAddr = (uint32_t)&SPI2->DR;
-    // DMA_InitStructure.DMA_PeripheralDataSize = DMA_PeripheralDataSize_Byte;
-    // DMA_InitStructure.DMA_PeripheralInc = DMA_PeripheralInc_Disable;
-    // DMA_InitStructure.DMA_MemoryBaseAddr = 0; // To be set later
-    // DMA_InitStructure.DMA_MemoryDataSize = DMA_MemoryDataSize_Byte;
-    // DMA_InitStructure.DMA_MemoryInc = DMA_MemoryInc_Enable;
-    // DMA_InitStructure.DMA_BufferSize = 40; // To be set later
-    // DMA_InitStructure.DMA_Mode = DMA_Mode_Normal;
-    // DMA_InitStructure.DMA_Priority = DMA_Priority_VeryHigh;
-    // DMA_InitStructure.DMA_M2M = DMA_M2M_Disable;
-    // DMA_Init(DMA1_Channel4, &DMA_InitStructure);
-
-    // // Configure SPI_BUS TX Channel
-    // DMA_InitStructure.DMA_DIR = DMA_DIR_PeripheralDST; // From memory to SPI
-    // DMA_InitStructure.DMA_PeripheralBaseAddr = (uint32_t)&SPI2->DR;
-    // DMA_InitStructure.DMA_PeripheralDataSize = DMA_PeripheralDataSize_Byte;
-    // DMA_InitStructure.DMA_PeripheralInc = DMA_PeripheralInc_Disable;
-    // DMA_InitStructure.DMA_MemoryBaseAddr = 0; // To be set later
-    // DMA_InitStructure.DMA_MemoryDataSize = DMA_MemoryDataSize_Byte;
-    // DMA_InitStructure.DMA_MemoryInc = DMA_MemoryInc_Enable;
-    // DMA_InitStructure.DMA_BufferSize = 40; // To be set later
-    // DMA_InitStructure.DMA_Mode = DMA_Mode_Normal;
-    // DMA_InitStructure.DMA_Priority = DMA_Priority_VeryHigh;
-    // DMA_InitStructure.DMA_M2M = DMA_M2M_Disable;
-    // DMA_Init(DMA1_Channel5, &DMA_InitStructure);
-
-    // SPI_I2S_DMACmd(SPI2, SPI_I2S_DMAReq_Rx, ENABLE);
-    // SPI_I2S_DMACmd(SPI2, SPI_I2S_DMAReq_Tx, ENABLE);
+    // readReg(VN_REG_YPR_IACC_ANGR, 36, NULL, VN_REQUEST);
   }
 
   /**
@@ -120,59 +88,34 @@ public:
     if (mode == VN_REQUEST_WAIT_READ || mode == VN_REQUEST) {
       // Request
       digitalWrite(csPin, LOW);
-
-      // Seems like VN100 likes a small gap between bytes. 
-      // Waiting for TXE delays by ~0.3us and that seems ok
-      SPI_SendData8(theSPI.SPIx, VN_CMD_READ);
-      while(!SPI_I2S_GetFlagStatus(theSPI.SPIx, SPI_I2S_FLAG_TXE));
-      SPI_SendData8(theSPI.SPIx, reg);
-      while(!SPI_I2S_GetFlagStatus(theSPI.SPIx, SPI_I2S_FLAG_TXE));
-      SPI_SendData8(theSPI.SPIx, 0);
-      while(!SPI_I2S_GetFlagStatus(theSPI.SPIx, SPI_I2S_FLAG_TXE));
-      SPI_SendData8(theSPI.SPIx, 0);
-      while(!SPI_I2S_GetFlagStatus(theSPI.SPIx, SPI_I2S_FLAG_TXE));
+      cmd[0] = VN_CMD_READ;
+      cmd[1] = reg;
+      _SPI.writeDMA(4, cmd);
+      // delayMicroseconds(1);//doesn't work if released too soon
       digitalWrite(csPin, HIGH);
       if (mode == VN_REQUEST_WAIT_READ) {
-        // need to wait 50 ms before response. Use this delay to clear the RX byte
-        uint32_t start = micros();
-        while (micros() - start < 50) {
-          if (SPI_I2S_GetFlagStatus(theSPI.SPIx, SPI_I2S_FLAG_RXNE))
-            SPI_ReceiveData8(theSPI.SPIx);
-        }
+        // need to wait 50 ms before response (SPI overhead adds some)
+        delayMicroseconds(45);
       }
       else if (mode == VN_REQUEST)
         return 0;
     }
 
     if (mode == VN_REQUEST_WAIT_READ || mode == VN_READ_REQUEST) {
+      uint8_t tempBuf[N+4];
       digitalWrite(csPin, LOW);
-      // delayMicroseconds(1);
-      uint8_t c;
-      for (int i=0; i<N+4; ++i) {
-        // c = theSPI.transfer(0x00);
-        SPI_SendData8(theSPI.SPIx, 0);
-        while(!SPI_I2S_GetFlagStatus(theSPI.SPIx, SPI_I2S_FLAG_RXNE));
-        c = SPI_ReceiveData8(theSPI.SPIx);
-
-        if (i<4)
-          resphead[i] = c;
-        else
-          buf[i-4] = c;
-      }
-      // delayMicroseconds(1);
+      _SPI.readDMA(N+4, tempBuf);
       digitalWrite(csPin, HIGH);
+      memcpy(resphead, tempBuf, 4);
+      memcpy(buf, &tempBuf[4], N);
       if (mode == VN_READ_REQUEST) {
         // Request again
-        delayMicroseconds(5);
+        delayMicroseconds(1);
         digitalWrite(csPin, LOW);
-        SPI_SendData8(theSPI.SPIx, VN_CMD_READ);
-        while(!SPI_I2S_GetFlagStatus(theSPI.SPIx, SPI_I2S_FLAG_TXE));
-        SPI_SendData8(theSPI.SPIx, reg);
-        while(!SPI_I2S_GetFlagStatus(theSPI.SPIx, SPI_I2S_FLAG_TXE));
-        SPI_SendData8(theSPI.SPIx, 0);
-        while(!SPI_I2S_GetFlagStatus(theSPI.SPIx, SPI_I2S_FLAG_TXE));
-        SPI_SendData8(theSPI.SPIx, 0);
-        while(!SPI_I2S_GetFlagStatus(theSPI.SPIx, SPI_I2S_FLAG_TXE));
+        cmd[0] = VN_CMD_READ;
+        cmd[1] = reg;
+        _SPI.writeDMA(4, cmd);
+        // delayMicroseconds(1);//doesn't work if released too soon
         digitalWrite(csPin, HIGH);
       }
       return resphead[3];
@@ -191,12 +134,12 @@ public:
   uint8_t writeReg(uint8_t reg, int N, const uint8_t *args) {
     digitalWrite(csPin, LOW);
     // delayMicroseconds(1);
-    theSPI.transfer(0x02);
-    theSPI.transfer(reg);
-    theSPI.transfer(0x00);
-    theSPI.transfer(0x00);
+    _SPI.transfer(0x02);
+    _SPI.transfer(reg);
+    _SPI.transfer(0x00);
+    _SPI.transfer(0x00);
     for (int i=0; i<N; ++i)
-      theSPI.transfer(args[i]);
+      _SPI.transfer(args[i]);
     // delayMicroseconds(1);
     digitalWrite(csPin, HIGH);
     delayMicroseconds(50);
@@ -207,23 +150,12 @@ public:
     // on the response packet to verify that the write register took effect, 
     // which is indicated by a zero error code."
     for (int i=0; i<4; ++i) {
-      resphead[i] = theSPI.transfer(0x00);
+      resphead[i] = _SPI.transfer(0x00);
     }
     // delayMicroseconds(1);
     digitalWrite(csPin, HIGH);
     return resphead[3];
   }
-
-
-  // void beginRead(uint8_t reg) {
-  //   digitalWrite(csPin, LOW);
-  //   theSPI.transfer(0x01);
-  //   theSPI.transfer(reg);
-  //   theSPI.transfer(0x00);
-  //   theSPI.transfer(0x00);
-  //   digitalWrite(csPin, HIGH);
-  //   delayMicroseconds(50);//VN100 minimum processing time=50
-  // }
 
   /**
    * @brief Retrieves angles and angular rates
@@ -243,51 +175,10 @@ public:
     // VN100: 27 (48bytes) = YPR,MAG,ACC,ANGRATES
     // VN100: 240 (36bytes) = YPR,TRUE_INERTIAL_ACC,ANGRATES
 
-    // NEW DMA ------------------------------
-
-    // digitalWrite(csPin, HIGH);
-    // DMA_Cmd(DMA1_Channel4, DISABLE);
-    // DMA_Cmd(DMA1_Channel5, DISABLE);
-    // // save data here
-    // yaw = radians(vn100240pkt.yaw);
-    // pitch = radians(vn100240pkt.pitch);
-    // roll = radians(vn100240pkt.roll);
-    // yawd = vn100240pkt.gyroz;
-    // pitchd = vn100240pkt.gyroy;
-    // rolld = vn100240pkt.gyrox;
-    // // begin next read
-    // // need small delay before asserting chip select again?
-    // delayMicroseconds(80);// 20 was too small
-    // beginRead(240);
-    // // set up DMA for receiving data
-    // // Prepare the DMA
-    // digitalWrite(csPin, LOW);
-    // DMA1_Channel5->CNDTR = sizeof(vn100240pkt);
-    // DMA1_Channel5->CMAR = (uint32_t)&vn100240pkt2;
-    // DMA1_Channel4->CNDTR = sizeof(vn100240pkt);
-    // DMA1_Channel4->CMAR = (uint32_t)&vn100240pkt;
-    // // Start transfer
-    // SPI_ReceiveData8(SPI2);//clear rxne flag
-    // DMA_Cmd(DMA1_Channel5, ENABLE);
-    // DMA_Cmd(DMA1_Channel4, ENABLE);
-
-    // OLD POLLING ----------------------
-
-    // VN200: 73 (72 bytes) = YPR,POS,VEL,ACC_BODY,ANGRATES
-    // static float dat[18];
-    // readReg(73, 72, (uint8_t *)dat);
-    // yaw = radians(dat[0]);
-    // pitch = radians(dat[1]);
-    // roll = radians(dat[2]);
-    // yawd = dat[17];
-    // pitchd = dat[16];
-    // rolld = dat[15];
-
-    // time polling: 350us @ clock div 4, 270us @ clock div 2
     static float dat[9];
     uint8_t errId = readReg(VN_REG_YPR_IACC_ANGR, 36, (uint8_t *)dat);
     // test read and request
-    // uint8_t errId = readReg(240, 36, (uint8_t *)dat, VN_READ_REQUEST);
+    // uint8_t errId = readReg(VN_REG_YPR_IACC_ANGR, 36, (uint8_t *)dat, VN_READ_REQUEST);
     // problems with reading?
     yaw = radians(dat[0]);
     pitch = radians(dat[1]);
